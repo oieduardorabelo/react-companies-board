275527b3d8a277557b9702fdeed0717c5c48dfcd
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-03-15 22:59:33 +1300

    lint and updates

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import Companies from './components/Companies'
 
 render(
-  <Companies/>,
+  <Companies />,
   document.getElementById('Companies')
 )
