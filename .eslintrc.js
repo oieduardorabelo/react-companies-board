62eed85cc58968201909cba59bb8a8ed62362fb1
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-08-30 23:01:37 +1200

    renaming to eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  parseOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    semi: [2, "never"]
+  }
+}
