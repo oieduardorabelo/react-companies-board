936733f141fbbbbf4aec0619951440ce5e8eda98
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-10-12 21:20:21 +1300

    new test

---
+++ src/components/Employee_mocha.jsx
@@ -0,0 +1,29 @@
+import expect from 'expect'
+import React from 'react'
+import shortid from 'shortid'
+import { mount } from 'enzyme'
+
+import Employee from './Employee'
+
+const generateEmployee = config => mount(<Employee {...config} />)
+
+const defaultConfig = {
+  companyId: '',
+  editMode: true,
+  email: '',
+  firstName: '',
+  id: 1,
+  lastName: '',
+}
+
+describe('Suite for <Employee />', () => {
+  describe('rendering in no-editMode', () => {
+    const underTest = generateEmployee(Object.assign(defaultConfig, { editMode: false }))
+
+    it('should render <ul> tag', () => {
+      const actual = underTest.find('ul').length
+      const result = 1
+      expect(actual).toBe(result)
+    })
+  })
+})
