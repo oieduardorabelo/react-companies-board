26b28d38d8c140502c2e3ae60af7d733e3533549
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-03-09 23:10:49 +1300

    editMode test for CompanyName

--- src/components/CompanyName.test.jsx
+++ src/components/CompanyName.test.jsx
@@ -16,4 +16,17 @@ describe('Suite for <CompanyName />', () => {
     )
     expect(underTest.find('h2').text()).toBe('HJK')
   })
+
+  it('should render in editMode', () => {
+    const underTest = mount(
+      <CompanyName
+        company={{
+          id: 'hjk',
+          name: 'HJK',
+        }}
+        editMode
+      />,
+    )
+    expect(underTest.find('.input-field').length).toBe(1)
+  })
 })
