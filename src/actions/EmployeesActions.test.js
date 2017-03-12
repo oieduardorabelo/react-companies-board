b031a5a3e1b75ecb1501e5eb19da51a567f048d8
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-03-12 23:46:26 +1300

    teardown for dispatch spy and unlinkCompany test

--- src/actions/EmployeesActions.test.js
+++ src/actions/EmployeesActions.test.js
@@ -4,12 +4,26 @@ import EmployeesActions from './EmployeesActions'
 AppDispatcher.dispatch = jest.fn()
 
 describe('Suite for <EmployeesActions />', () => {
+  afterEach(() => {
+    AppDispatcher.dispatch.mockReset()
+  })
+
   it('dipsatch linkCompany correctly', () => {
     EmployeesActions.linkCompany('123companyId')
 
     expect(AppDispatcher.dispatch).toHaveBeenCalledTimes(1)
     expect(AppDispatcher.dispatch).toHaveBeenCalledWith({
       actionType: 'LINK_COMPANY',
+      companyId: '123companyId',
+    })
+  })
+
+  it('dipsatch unlinkCompany correctly', () => {
+    EmployeesActions.unlinkCompany('123companyId')
+
+    expect(AppDispatcher.dispatch).toHaveBeenCalledTimes(1)
+    expect(AppDispatcher.dispatch).toHaveBeenCalledWith({
+      actionType: 'UNLINK_COMPANY',
       companyId: '123companyId',
     })
   })
